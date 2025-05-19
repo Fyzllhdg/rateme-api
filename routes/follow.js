c026e5da213dcd775f531333c3dd1c54d06fb6d6
Fyzllhdg <doganfeyzullah57@gmail.com>
2025-05-19 23:35:35 +0300

    app.use(cors());  eklendi

--- routes/follow.js
+++ routes/follow.js
@@ -2,7 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const db = require('../config/db'); // MySQL bağlantısı
+
 const authenticateToken = require('../middleware/auth');
 
 // Takip etme isteği gönderme
